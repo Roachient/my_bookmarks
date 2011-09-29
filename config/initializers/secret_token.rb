5b286d4b57a5cf6fa04e1d7e0b38e0f0ed296db3
Rodney Arnett <rodney@rodneygarnett.com>
2011-09-29 11:04:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBookmarks::Application.config.secret_token = 'e1ecf52c39fa67068eede1ba9e399d8e5aa00665d5342c2e410c571417e58ce430877bf120434454b619c9165b004d1167daa4376992f7393ce0276fa1de90e6'
